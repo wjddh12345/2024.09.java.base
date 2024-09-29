8611ab11deb44eb3796f80135ef1581e1f304bbb
wjddh12345 <wjddh1121@naver.com>
2024-09-29 19:30:04 +0900

    스캐너 입력받기

---
+++ src/com/pjo/java/ssg/Main.java
@@ -0,0 +1,18 @@
+package com.pjo.java.ssg;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("명령어) ");
+		String command;
+		command = sc.next();
+		System.out.printf("입력된 명령어 : %s\n", command);
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
