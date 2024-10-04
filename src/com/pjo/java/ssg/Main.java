f948255be0b24c28ad355bc00d63a06347012137
wjddh12345 <wjddh1121@naver.com>
2024-10-05 07:54:28 +0900

    조회수 기능 구현

--- src/com/pjo/java/ssg/Main.java
+++ src/com/pjo/java/ssg/Main.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
+		
+		makeTestArticles();
 
 		Scanner sc = new Scanner(System.in);
 
@@ -51,11 +53,11 @@ public class Main {
 					continue;
 				}
 				
-				System.out.println("번호, 제목");
+				System.out.println("번호 | 조회 | 제목");
 				for ( int i = articles.size() - 1; i >= 0; i-- ) {
 					Article article = articles.get(i);
 					
-					System.out.printf("%d, %s\n", article.id, article.title);
+					System.out.printf("%4d | %4d | %3s\n", article.id, article.hit, article.title);
 				}
 			}
 			
@@ -80,11 +82,14 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
+				
+				foundArticle.increaseHit();
 
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+				System.out.printf("내용 : %s\n", foundArticle.hit);
 				continue;
 
 			}
@@ -154,6 +159,10 @@ public class Main {
 
 		System.out.println("== 프로그램 끝 ==");
 	}
+
+	private static void makeTestArticles() {
+		
+	}
 }
 
 class Article {
@@ -161,11 +170,17 @@ class Article {
 	String regDate;
 	String title;
 	String body;
+	int hit;
 	
 	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
 		this.regDate = regDate;
+		this.hit = 0;
+	}
+	
+	public void increaseHit() {
+		hit++;
 	}
 }
