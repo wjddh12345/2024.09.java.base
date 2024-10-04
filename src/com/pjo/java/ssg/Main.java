eb5e247924d8e1f5e6c842a0c838add809a9dd33
wjddh12345 <wjddh1121@naver.com>
2024-10-05 01:31:51 +0900

    게시물 상세페이지 기능 구현

--- src/com/pjo/java/ssg/Main.java
+++ src/com/pjo/java/ssg/Main.java
@@ -58,6 +58,33 @@ public class Main {
 				}
 			}
 			
+			else if ( command.startsWith("article detail ") ) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+				
+				Article foundArticle = null;
+				
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2024-10-05 1:1:1");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+				continue;
+
+			}
 			else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
