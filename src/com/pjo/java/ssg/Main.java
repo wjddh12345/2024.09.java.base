5ef3f34d9c2b32c0791283758d4f2eee6416b443
wjddh12345 <wjddh1121@naver.com>
2024-10-07 12:31:01 +0900

    테스트 데이터 생성 메서드 구현 및 적용

--- src/com/pjo/java/ssg/Main.java
+++ src/com/pjo/java/ssg/Main.java
@@ -5,16 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	
+	static List<Article> articles;
+	
+	static {
+	     articles = new ArrayList();
+	}
+	
 	public static void main(String[] args) {
+		
 		System.out.println("== 프로그램 시작 ==");
 		
-		makeTestArticles();
+		makeTestData();
 
 		Scanner sc = new Scanner(System.in);
-
-		int lastArticleId = 0;
-		
-		List<Article> articles = new ArrayList();
 
 		while (true) { 
 			System.out.printf("명령어) ");
@@ -31,8 +35,8 @@ public class Main {
 			} 
 			
 			else if (command.equals("article write")) {
-				int id = lastArticleId + 1;
-
+				int id = articles.size() + 1;
+				
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
@@ -42,8 +46,6 @@ public class Main {
 				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 				
-				lastArticleId = id;
-
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} 
 			
@@ -160,8 +162,11 @@ public class Main {
 		System.out.println("== 프로그램 끝 ==");
 	}
 
-	private static void makeTestArticles() {
-		
+	private static void makeTestData() {
+		System.out.println("테스트를 위한 데이터를 생성합니다.");
+		articles.add(new Article(1, Util.getNowDateStr(), "제목1", "내용1", 10));
+		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 20));
+		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 100));
 	}
 }
 
@@ -173,11 +178,15 @@ class Article {
 	int hit;
 	
 	public Article(int id, String regDate, String title, String body) {
+		this(id, regDate, title, body, 0);
+	}
+	
+	public Article(int id, String regDate, String title, String body, int hit) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
 		this.regDate = regDate;
-		this.hit = 0;
+		this.hit = hit;
 	}
 	
 	public void increaseHit() {
