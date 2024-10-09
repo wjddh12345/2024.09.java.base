1b8e83d8141c5f9ba4818c6f0cdaa037954dad77
wjddh12345 <wjddh1121@naver.com>
2024-10-09 20:07:03 +0900

    회원 가입 기능 구현

--- src/com/pjo/java/ssg/App.java
+++ src/com/pjo/java/ssg/App.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.pjo.java.ssg.dto.Article;
+import com.pjo.java.ssg.dto.Member;
 import com.pjo.java.ssg.util.Util;
 
 public class App {
-	List<Article> articles;
+	private List<Article> articles;
+	private List<Member> members;
 	
-	App() {
+	public App() {
 		articles = new ArrayList();
+		members = new ArrayList();
 	}
 	
 	public void start() {
@@ -35,15 +38,56 @@ public class App {
 				if (command.equals("system exit")) {
 					break;
 				} 
+				else if (command.equals("member join")) {
+					int id = members.size() + 1;
+					String regDate = Util.getNowDateStr();
+					
+					String loginId = null;
+					String loginPw = null;
+					String loginPwConfirm = null;
+					
+					while (true) {
+						System.out.printf("로그인 아이디 : ");
+						loginId = sc.nextLine();
+						
+						if (isJoinableLoginId(loginId) == false) {
+							System.out.printf("%s은 이미 사용중인 아이디입니다.\n", loginId);
+							continue;
+						}
+						
+						break;
+					}
 				
+					while (true) {
+						System.out.printf("로그인 비밀번호 : ");
+						loginPw = sc.nextLine();
+						System.out.printf("로그인 비밀번호 확인 : ");
+						loginPwConfirm = sc.nextLine();
+
+						if (loginPw.equals(loginPwConfirm) == false ) {
+							System.out.println("비밀번호를 다시 입력해주세요");
+							continue;
+						}
+						
+						break;
+					}
+					
+					System.out.printf("이름 : ");
+					String name = sc.nextLine();
+					
+					Member member = new Member(id, regDate, loginId, loginPw, name);
+					members.add(member);
+					
+					System.out.printf("%d번 회원이 생성되었습니다.\n", id);
+				} 
+
 				else if (command.equals("article write")) {
 					int id = articles.size() + 1;
-					
+					String regDate = Util.getNowDateStr();
 					System.out.printf("제목 : ");
 					String title = sc.nextLine();
 					System.out.printf("내용 : ");
 					String body = sc.nextLine();
-					String regDate = Util.getNowDateStr();
 
 					Article article = new Article(id, regDate, title, body);
 					articles.add(article);
@@ -131,6 +175,30 @@ public class App {
 			sc.close();
 			System.out.println("== 프로그램 끝 ==");
 		}
+
+	private boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+		
+		if ( index == -1 ) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		
+		for (Member member : members) {
+		    if (member.loginId.equals (loginId)) {
+		        return i;
+		    }
+		    
+		    i++;
+		}
+		
+		return -1;
+	}
 
 	private int getArticleIndexById(int id) {
 		int i = 0;
